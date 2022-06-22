c3fe9320aed6737e179d4b8034e1dccd31e46314
MuQiaoYu <93062875+nefelibetas@users.noreply.github.com>
2022-06-22 20:52:41 +0800

    Add files via upload

---
+++ 实验/graph.h
@@ -0,0 +1,409 @@
+#ifndef GRAPH_H_INCLUDED
+#define GRAPH_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#define N 6
+//邻接矩阵数据结构
+typedef struct
+{
+    int vcount;     //顶点数
+    int type;       // 0 无向图，1 有向图
+    char vexs[N];   // 顶点信息
+    int arcs[N][N]; //关系矩阵
+} GraphMatrix;
+
+//邻接表数据结构
+struct EdgeNode
+{
+    int endvex;                //对应顶点的边表下标
+    int weight;                //边的权
+    struct EdgeNode *nextedge; //链字段
+};
+typedef struct EdgeNode *EdgeList;
+
+//顶点表
+typedef struct
+{
+    char vertex;       //记录顶点的信息
+    int degree;        //用于记录顶点的入度，在拓扑排序时需使用
+    EdgeList edgelist; //指向边表的指针
+} VexNode;
+
+//邻接表
+typedef struct
+{
+    VexNode vexs[N]; // N个顶点
+    int type;        // 0无向图，1有向图
+    int vcount;      //顶点数
+} GraphList;
+
+/*第一关 完成图初始化*/
+void printGraph(GraphMatrix *G)
+{ //本函数输出图的邻接矩阵
+    int i, j;
+    for (i = 0; i < G->vcount; i++)
+    {
+        // printf("%c ",G->vexs[i]);
+        for (j = 0; j < G->vcount; j++)
+            printf("%d ", G->arcs[i][j]);
+        printf("\n");
+    }
+}
+
+/*第一关 完成图初始化-邻接矩阵*/
+GraphMatrix *initGraphMatrix()
+{
+    /*
+    第一行输入图的类型、图的顶点数和边数
+    第二行输入各条边的两顶点的编号
+    按顶点编号从小到大的顺序输入
+    输出邻接矩阵，不需要考虑输入的数字非法情况
+    不输入顶点的信息
+    */
+    GraphMatrix *map = (GraphMatrix *)malloc(sizeof(GraphMatrix));
+    int n;
+    scanf("%d%d%d", &map->type, &map->vcount, &n);
+
+    for (int i = 0; i < map->vcount; i++)
+        for (int j = 0; j < map->vcount; j++)
+            map->arcs[i][j] = 0;
+    //初始化关系矩阵
+    int i = 0;
+    while (i < n)
+    {
+        int j, k;
+        scanf("%d%d", &j, &k);
+        map->arcs[j][k] = 1;
+        if(map->type==0)
+        {
+            map->arcs[k][j]=1;
+        }
+        i++;
+    }
+    return map;
+}
+/*第二关 完成图初始化-邻接表,并完成输出图的邻接表函数*/
+GraphList *initGraphList()
+{
+    /*
+    第一行输入图的类型、图的顶点数和边数
+    第二行输入顶点（对应下标）信息
+    第三行输入各条边的两顶点的编号
+    按顶点编号从小到大的顺序输入。
+    输出邻接表。
+    不需考虑输入的数字非法情况
+    输入顶点的信息
+    */
+    GraphList *map = (GraphList *)malloc(sizeof(GraphList));
+    int n;
+    scanf("%d%d%d", &map->type, &map->vcount, &n);
+
+    char c[map->vcount];
+    gets(c);
+    gets(c);
+    //第一个吸收空格的
+    for(int i=0; i<map->vcount; i++)
+    {
+        map->vexs[i].vertex =c[i];
+    }
+    //输入顶点数据
+
+    int j,k;
+    int i=0;
+    for(int i=0;i<map->vcount;i++)
+    {
+        map->vexs[i].edgelist=NULL;
+        //map->vexs[i].edgelist->weight=0;
+    }
+    while(i<n)
+    {
+        scanf("%d%d",&j,&k);
+        EdgeList s=(EdgeList)malloc(sizeof(struct EdgeNode));
+        s->endvex=k;
+        s->nextedge=map->vexs[j].edgelist;
+        map->vexs[j].edgelist=s;
+        if(map->type==0)
+        {
+            EdgeList s=(EdgeList)malloc(sizeof(struct EdgeNode));
+            s->endvex=j;
+            s->nextedge=map->vexs[k].edgelist;
+            map->vexs[k].edgelist=s;
+        }
+        i++;
+    }
+    return map;
+}
+
+void printGraphLit(GraphList *G)
+{
+    /*输出邻接表，输出格式：顶点->邻接顶点编号->...*/
+    for (int i = 0; i < G->vcount; i++)
+    {
+        EdgeList p = G->vexs[i].edgelist;
+        printf("%c", G->vexs[i].vertex);
+        while (p != NULL)
+        {
+            printf("->%d", p->endvex);
+            p=p->nextedge;
+        }
+        printf("\n");
+    }
+}
+
+/*第三关 完成图的广度遍历（周游），可根据需要添加自定义函数*/
+int visited[N];
+void ClearFlag(GraphList *G)
+{
+    for(int i=0;i<G->vcount;i++)
+        visited[i]=0;
+}
+struct SeqQueue
+{
+    int f,r;
+    int len;
+    int *Q;
+};
+typedef struct SeqQueue *PQueue;
+
+void InitQueue(PQueue q,GraphList *G)
+{
+    q->f=0;
+    q->r=0;
+    q->len=0;
+    q->Q=(int *)malloc(sizeof(int)*G->vcount);
+}
+void EnQueue(PQueue q,int index)
+{
+    if(q->r==q->len-1)
+    {
+        printf("Out of Space");
+        return;
+    }
+    q->Q[q->r]=index;
+    q->r=q->r+1;
+    q->len=q->len+1;
+}
+void DeQueue(PQueue q)
+{
+    q->Q[q->r]=0;
+    q->r=q->r-1;
+    q->len=q->len-1;
+}
+void BFS(GraphList *G,int index)
+{
+    EdgeList p;
+    PQueue WaitingQueue=(PQueue)malloc(sizeof(struct SeqQueue));
+    InitQueue(WaitingQueue,G);
+    if(visited[index]==1)
+        return;
+    printf("%d ",index);
+    visited[index]=1;
+    EnQueue(WaitingQueue,index);
+    p=G->vexs[index].edgelist;
+    while(WaitingQueue->len!=0)
+    {
+        DeQueue(WaitingQueue);
+        while(p)
+        {
+           if(visited[p->endvex]==1)
+                p=p->nextedge;
+           else if(visited[p->endvex]==0)
+           {
+               visited[p->endvex]=1;
+               EnQueue(WaitingQueue,p->endvex);
+               printf("%d ",p->endvex);
+           }
+        }
+    }
+}
+
+void BFS_list(GraphList *G)
+{
+    ClearFlag(G);
+    for(int i=0;i<G->vcount;i++)
+        if(!visited[i])
+            BFS(G,i);
+}
+/*第四关 完成图的深度遍历（周游），可根据需要添加自定义函数 */
+
+void DFS(GraphList *G,int index)
+{
+    EdgeList p=G->vexs[index].edgelist;
+    if(visited[index]==1)
+        return;
+    printf("%d ",index);
+    visited[index]=1;
+    while(p&&visited[p->endvex]==1)
+        p=p->nextedge;
+    if(p)
+    {
+        index=p->endvex;
+        DFS(G,index);
+    }
+}
+void DFS_list(GraphList *G)
+{
+    ClearFlag(G);
+    for(int i=0;i<G->vcount;i++)
+        if(!visited[i])
+            DFS(G,i);
+}
+
+/*第五关 生成图的拓扑排序，可根据需要添加自定义函数
+*/
+
+void Top_list(GraphList *G)
+{
+	for(int j=0;j<G->vcount-1;j++)
+	{
+		for(int i=0;i<G->vcount;i++)
+		{
+			if(G->vexs[i].degree == -1)
+			{
+				continue;
+			}
+			else if(G->vexs[i].degree == 0)
+			{
+				G->vexs[i].degree = -1;
+				char temp = G->vexs[i].vertex;
+				printf("%c ",temp);
+				struct EdgeNode *t = G->vexs[i].edgelist;
+				while(t!=NULL)
+				{
+					char tc = t->endvex + '0';
+					for(int k=0;k<G->vcount;k++)
+					{
+						if(G->vexs[k].degree == -1)
+						{
+							continue;
+						}
+						else if(G->vexs[k].vertex == tc)
+						{
+							G->vexs[k].degree--;
+						}
+					}
+					t = t->nextedge;
+				}
+			}
+
+		}
+	}
+}
+
+/*第六关 prim算法生成最小生成树*/
+struct mst{
+	int start;
+	int stop;
+	int weight;
+};
+
+struct hmst{
+	struct mst msts[N];
+	int total;
+};
+
+void Prim_list(GraphList *G)
+{
+
+	struct hmst *H = (struct hmst *)malloc(sizeof(struct hmst));//待加入的边
+	struct hmst *T = (struct hmst *)malloc(sizeof(struct hmst));//加入的边
+	T->total = 0;
+	H->total = 0;
+	int sign[N];//标记点
+	//memset(sign,1,sizeof sign);
+	for(int i=0;i<N;i++)
+	{
+		sign[i] = 1;
+	}
+	int low = 0;
+	int low_s = 0;
+	int lowp = 0;
+	struct EdgeNode *t = G->vexs[0].edgelist;
+	sign[0] = 0;
+	while(t!=NULL)
+	{
+		printf("addH 0 %d %d \n",t->weight,t->endvex);
+		H->msts[H->total].start = 0;
+		H->msts[H->total].stop = t->endvex;
+		H->msts[H->total].weight = t->weight;
+		H->total++;
+		t = t->nextedge;
+	}
+	for(int i=0;i<G->vcount-1;i++)
+	{
+		printf("1--------------------1\n");
+		low = H->msts[0].weight;
+		//确定待选边中的最小边
+		for(int j=0;j<H->total;j++)
+		{
+			if(H->msts[j].weight< low)
+			{
+				low = H->msts[j].weight;
+				low_s = j;
+			}
+		}
+		lowp = H->msts[low_s].stop;
+		printf("%d",lowp);
+		//添加到生成树
+		T->msts[T->total].start = H->msts[low_s].start;
+		T->msts[T->total].stop = H->msts[low_s].stop;
+		T->msts[T->total].weight = H->msts[low_s].weight;
+		sign[lowp] = 0;
+		printf("%d ",low_s);
+		printf("addT %d %d %d\n",T->msts[T->total].start,T->msts[T->total].stop,T->msts[T->total].weight);
+		T->total++;
+		//去除待选边中被选中的
+		for(int j=0;j<6;j++)
+		{
+			printf("%d",sign[j]);
+		}
+		printf("\n");
+		H->total--;
+		H->msts[low_s] = H->msts[H->total];
+		//去除待选边中不可能去选的（选了就成回路）
+		for(int j=0;j<H->total;j++)
+		{
+			if(sign[H->msts[j].stop]==0 && sign[H->msts[j].start] == 0)
+			{
+				H->total--;
+				H->msts[j] = H->msts[H->total];
+				j--;
+
+			}
+		}
+		//添加新的边到待选边
+		t = G->vexs[lowp].edgelist;
+		while(t!=NULL)
+		{
+			if(sign[t->endvex] != 0)
+			{
+				printf("addH %d %d %d\n",lowp,t->weight,t->endvex);
+				printf("%d",lowp);
+				H->msts[H->total].start = lowp;
+				H->msts[H->total].stop = t->endvex;
+				H->msts[H->total].weight = t->weight;
+				H->total++;
+			}
+			t = t->nextedge;
+		}
+	}
+	printf("\n");
+	for(int i=0;i<T->total;i++)
+	{
+		printf("%d %d\n",T->msts[i].start,T->msts[i].stop);
+	}
+}
+/*第七关 Kruskal算法生成最小生成树*/
+
+void Kruskal_list(GraphList *G)
+{
+
+}
+
+/*第八关 Dijistra算法求最短路径*/
+
+void Dijkstra_list(GraphList *G)
+{
+
+}
+#endif // GRAPH_H_INCLUDED
